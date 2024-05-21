ad20bbe563e82219d977c94736396c0cfb6281f9
Lucas Nascimento <lsnascimento.dev@gmail.com>
2024-05-21 18:52:32 -0300

    Production config

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({
-  port: env.PORT
+  port: env.PORT,
+  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 }).then(() => {
   console.log('HTTP Running!')
 })
